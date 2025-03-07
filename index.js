b50dc15e3c3fc6a3665a3c7d87cf307ee62b06c4
ArtRosh <artem.rosh17@gmail.com>
2025-03-06 21:15:12 -0700

    lab

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books") // Возвращаем fetch()
+        .then(response => response.json()) 
+        .then(data => renderBooks(data))
+        .catch(error => console.error("Ошибка запроса:", error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main') || document.body; // Проверяем наличие <main>
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name; // Используем textContent
     main.appendChild(h2);
   });
 }
